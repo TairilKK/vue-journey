7c91666697a2271b3cd4f20d4c3495e200cde55e
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-06-29 21:11:03 +0200

    attribute binding

--- cdn/app.js
+++ cdn/app.js
@@ -1,11 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://vuejs.org/",
       showBooks: true,
       books: [
-        { title: "Zoo", author: "Author 1" },
-        { title: "Monkey", author: "Author 2" },
-        { title: "Donkey", author: "Author 3" },
+        { title: "Zoo", author: "Author 1", img: "https://picsum.photos/200" },
+        {
+          title: "Monkey",
+          author: "Author 2",
+          img: "https://picsum.photos/200",
+        },
+        {
+          title: "Donkey",
+          author: "Author 3",
+          img: "https://picsum.photos/200",
+        },
       ],
     };
   },
