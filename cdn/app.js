3a5945b8a5d6c3af308500d5c0028ffc7c7276e8
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-06-29 21:19:11 +0200

    dynamic class

--- cdn/app.js
+++ cdn/app.js
@@ -4,16 +4,23 @@ const app = Vue.createApp({
       url: "https://vuejs.org/",
       showBooks: true,
       books: [
-        { title: "Zoo", author: "Author 1", img: "https://picsum.photos/200" },
+        {
+          title: "Zoo",
+          author: "Author 1",
+          img: "https://picsum.photos/200",
+          isFavorite: false,
+        },
         {
           title: "Monkey",
           author: "Author 2",
           img: "https://picsum.photos/200",
+          isFavorite: true,
         },
         {
           title: "Donkey",
           author: "Author 3",
           img: "https://picsum.photos/200",
+          isFavorite: true,
         },
       ],
     };
