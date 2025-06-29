48922a536cbd8c5ffa24afe86d6ea93d1cbcd6ce
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-06-29 21:00:25 +0200

    mouse events

--- cdn/app.js
+++ cdn/app.js
@@ -5,12 +5,24 @@ const app = Vue.createApp({
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45,
+      x: 0,
+      y: 0,
     };
   },
 
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
