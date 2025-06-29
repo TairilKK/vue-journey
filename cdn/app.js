3773a9820ab7fa56c3e4408c78c6baf111a9cab5
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-06-29 21:03:40 +0200

    output lists

--- cdn/app.js
+++ cdn/app.js
@@ -2,27 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        { title: "Zoo", author: "Author 1" },
+        { title: "Monkey", author: "Author 2" },
+        { title: "Donkey", author: "Author 3" },
+      ],
     };
   },
 
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
-    },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) {
-        console.log(data);
-      }
-    },
-    handleMouseMove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
     },
   },
 });
