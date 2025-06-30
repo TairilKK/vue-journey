b5bf8a88c953eae315725f9b0fbc4bc54f7bdae6
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-06-30 16:01:59 +0200

    create: Modal component

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <Modal theme="red" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+
+      <h1>Movie title</h1>
+      <p>Some description</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">open modal(alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "Vue 3 Modal Project",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="modal" @click.self="closeModal">
+    <div class="modal-content" :class="{ red: theme === 'red' }">
+      '{{ theme }}'
+      <slot></slot>
+      <slot name="links"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal .modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 500px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+.modal .modal-content.red {
+  background-color: #ff0000;
+  border-color: #ff0000;
+}
+</style>
