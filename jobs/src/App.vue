723b02d44d01015455d1e4f59daad97002f5278a
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-03 18:31:20 +0200

    moving back, forward and redirect

--- jobs/src/App.vue
+++ jobs/src/App.vue
@@ -4,9 +4,29 @@
     <router-link :to="{ name: 'about' }">About</router-link>
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
   <router-view />
 </template>
 
+<script>
+export default {
+  name: "App",
+  methods: {
+    redirect() {
+      this.$router.push({ name: "home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -31,5 +51,12 @@ nav a {
 nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+button {
+  padding: 10px;
+  border-radius: 4px;
+  margin: 0 10px;
+  border: none;
+  cursor: pointer;
 }
 </style>
