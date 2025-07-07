0d2c1457d3f1404b7ba1d3bdff1cd2e369d1aa5d
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-07 13:52:43 +0200

    computed property

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Ref</h2>
-    <p>My name is {{ userOne.name }} my age is {{ userOne.age }}</p>
-    <button @click="updateUserOne">Click me</button>
-
-    <h2>Reactive</h2>
-    <p>My name is {{ userTwo.name }} my age is {{ userTwo.age }}</p>
-    <button @click="updateUserTwo">Click me</button>
+    <input type="text" v-model="search" />
+    <p>{{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { computed, ref } from "vue";
 
 export default {
   name: "HomeView",
   setup() {
-    const userOne = ref({ name: "mario", age: 30 });
-    const userTwo = reactive({ name: "mario", age: 35 });
+    const search = ref("");
+    const names = ref([
+      "Anna",
+      "Bartek",
+      "Celina",
+      "Damian",
+      "Ewa",
+      "Filip",
+      "Grzegorz",
+      "Hanna",
+      "Igor",
+      "Julia",
+    ]);
 
-    const updateUserOne = () => {
-      userOne.value.name = "luigi";
-    };
+    const matchingNames = computed(() =>
+      names.value.filter((n) => n.includes(search.value))
+    );
 
-    const updateUserTwo = () => {
-      userTwo.name = "luigi";
-    };
-    return { userOne, updateUserOne, userTwo, updateUserTwo };
+    return { names, search, matchingNames };
   },
 };
 </script>
