00d07d7f6571a022329005a9ccb55a6e1ecad59c
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-07 13:44:44 +0200

    ref vs reactive

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <h2>Ref</h2>
     <p>My name is {{ userOne.name }} my age is {{ userOne.age }}</p>
     <button @click="updateUserOne">Click me</button>
-    <button @click="userOne.age++">Add 1 to age</button>
-    <input type="text" v-model="userOne.name" />
+
+    <h2>Reactive</h2>
+    <p>My name is {{ userTwo.name }} my age is {{ userTwo.age }}</p>
+    <button @click="updateUserTwo">Click me</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
   name: "HomeView",
   setup() {
-    let userOne = ref({ name: "mario", age: 30 });
+    const userOne = ref({ name: "mario", age: 30 });
+    const userTwo = reactive({ name: "mario", age: 35 });
 
     const updateUserOne = () => {
       userOne.value.name = "luigi";
     };
 
-    return { userOne, updateUserOne };
+    const updateUserTwo = () => {
+      userTwo.name = "luigi";
+    };
+    return { userOne, updateUserOne, userTwo, updateUserTwo };
   },
 };
 </script>
