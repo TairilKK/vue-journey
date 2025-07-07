f620fb74b13ec87c7b11a99b4494e5a4fcd431ad
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-07 13:24:18 +0200

    use of composition api

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    Home
+    <p ref="p">My name is {{ name }} my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
+  setup() {
+    console.log("setup");
+
+    const p = ref(null);
+
+    // not reactive
+    let name = "mario";
+    let age = "age";
+
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add("test");
+    };
+
+    return { name, age, handleClick, p };
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
 };
 </script>
