7d2b54e7f34327314e116484049cbdba983d4bcb
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-08 14:49:19 +0200

    add: some styles

--- dojo-blog/src/App.vue
+++ dojo-blog/src/App.vue
@@ -7,7 +7,9 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+a {
+  all: unset;
 }
 </style>
