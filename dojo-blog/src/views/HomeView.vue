37cf47cf2a141b8025e2dc38e6a5c6ec4b8c022f
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-08 13:51:50 +0200

    add: composoable getPosts and render tags

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -13,27 +13,13 @@
 
 <script>
 import PostList from "@/components/PostList.vue";
-import { ref } from "vue";
+import { getPosts } from "@/composables/getPosts";
 
 export default {
   name: "HomeView",
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-
-        if (!data.ok) {
-          throw new Error("no data available");
-        }
-        posts.value = await data.json();
-      } catch (err) {
-        error.value = err.message;
-        console.error(error.value);
-      }
-    };
+    const { posts, error, load } = getPosts();
     load();
     return { posts, error };
   },
